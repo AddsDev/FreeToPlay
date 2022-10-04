4f92edfe5032028578d29f947d158d405dff9099
Adrian Ruiz <adrian.ruiz@evertecinc.com>
2022-10-03 20:47:00 -0500

    Update ktor client

--- app/src/main/java/dev/adds/freetoplay/network/KtorClient.kt
+++ app/src/main/java/dev/adds/freetoplay/network/KtorClient.kt
@@ -1,9 +1,11 @@
 package dev.adds.freetoplay.network
 
-import android.util.Log
 import com.google.gson.Gson
+import com.readystatesoftware.chuck.ChuckInterceptor
+import io.github.aakira.napier.Napier
 import io.ktor.client.*
 import io.ktor.client.engine.android.*
+import io.ktor.client.engine.okhttp.*
 import io.ktor.client.plugins.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.plugins.logging.*
@@ -11,10 +13,13 @@ import io.ktor.client.request.*
 import io.ktor.http.*
 import io.ktor.serialization.gson.*
 
+
 object KtorClient {
     private const val KTOR_TAG: String = "Ktor-Log"
+
     val httpClient = HttpClient {
         install(ContentNegotiation) {
+
             gson {
                 Gson().newBuilder()
                     .setPrettyPrinting()
@@ -22,20 +27,33 @@ object KtorClient {
                     .create()
             }
             Logging {
-                level = LogLevel.ALL
+                level = LogLevel.HEADERS
                 logger = object : Logger {
                     override fun log(message: String) {
-                        Log.i(KTOR_TAG, message)
+                        Napier.v("HTTP Client", null, message)
                     }
-
+                }
+                filter {
+                    it.url.host.contains("freetogame")
                 }
             }
-            HttpClient(Android) {
+            /*HttpClient(Android) {
                 engine {
                     connectTimeout = 100_00
                     socketTimeout = 100_00
-                    //For OkHttp
-                    //https://ktor.io/docs/eap/http-client-engines.html#okhttp
+                }
+            }*/
+            //For OkHttp
+            //https://ktor.io/docs/eap/http-client-engines.html#okhttp
+            HttpClient(OkHttp){
+                engine {
+                    config {
+                        followRedirects(true)
+                    }
+                    /*addInterceptor(ChuckInterceptor(context))
+                    addNetworkInterceptor(interceptor)
+
+                    preconfigured = okHttpClientInstance*/
                 }
             }
             defaultRequest {
